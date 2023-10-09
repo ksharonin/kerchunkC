06f0824c5b9d4f6cd0dd278fe035d801c555b523
ksharonin <ksharonin@berkeley.edu>
2023-10-09 16:35:54 -0700

    init commit for setup

---
+++ code/python/range_req.py
@@ -0,0 +1,9 @@
+""" range_ req.py
+
+    manually execute S3 range-get requests 
+    lower-level Python code (e.g., Python AWS SDK/boto) 
+    decompress and decode them into numpy arrays 
+    using the information in the Kerchunk reference files 
+    (byte order, dimensionality, etc.). 
+"""
+
