d073cdd0d2dcb6cb29f55c880fc964e826d076d0
ksharonin <ksharonin@berkeley.edu>
2023-12-06 19:45:23 -0800

    complete dataset path and variable integration

--- code/c++/include/config.h
+++ code/c++/include/config.h
@@ -27,13 +27,26 @@
 #define USE_LOCAL true
 
 // path to metadata JSON
-#define HARDCODED_JSON_PATH "/Users/katrinasharonin/Downloads/kerchunkC/jsons/2023-006.json" // "/Users/katrinasharonin/Downloads/kerchunkC/code/jupyter/01_air_pressure_at_mean_sea_level.json"
+#define HARDCODED_JSON_PATH "/Users/katrinasharonin/Downloads/kerchunkC/jsons/2023-006.json"
+// "/Users/katrinasharonin/Downloads/kerchunkC/jsons/ATL03_REF.json" 
+// "/Users/katrinasharonin/Downloads/kerchunkC/code/jupyter/01_air_pressure_at_mean_sea_level.json"
+// "/Users/katrinasharonin/Downloads/kerchunkC/jsons/2023-006.json"
+
 // hardcoded byte stream source local to device
-#define HARCODED_LOCAL_NC_PATH "/Users/katrinasharonin/Downloads/GOES_17_recreation/GOES_17_Sample_Data/2023/006/05/OR_ABI-L1b-RadF-M6C01_G17_s20230060500309_e20230060509376_c20230060509434.nc" //  "/Users/katrinasharonin/Downloads/2020_01_data_air_pressure_at_mean_sea_level.nc"
+#define HARCODED_LOCAL_NC_PATH "/Users/katrinasharonin/Downloads/GOES_17_recreation/GOES_17_Sample_Data/2023/006/05/OR_ABI-L1b-RadF-M6C01_G17_s20230060500309_e20230060509376_c20230060509434.nc"
+// "/Users/katrinasharonin/Downloads/2020_01_data_air_pressure_at_mean_sea_level.nc"
+// "/Users/katrinasharonin/Downloads/GOES_17_recreation/GOES_17_Sample_Data/2023/006/05/OR_ABI-L1b-RadF-M6C01_G17_s20230060500309_e20230060509376_c20230060509434.nc" 
+// "/Users/katrinasharonin/Downloads/2020_01_data_air_pressure_at_mean_sea_level.nc"
+
 // hardcoded dataset name/path in file, leave as "" if not applicable
 #define HARDCODED_DATASET_NAME ""
+// "/quality_assessment/gt2l/"
+
 // harcoded variable; if single variable you can leave as ""
-#define HARCODED_VARIABLE "Rad" //  ""
+#define HARDCODED_VARIABLE "Rad"
+// "qa_perc_signal_conf_ph_low" 
+// "air_pressure_at_mean_sea_level" 
+// "Rad"
 
 // inside of a chunk e.g. size {24, 100, 100} visit at index {x,y,z}
 #define HARDCODED_TEST_VISIT {12, 33, 10}
--- code/c++/include/json_parse.h
+++ code/c++/include/json_parse.h
@@ -16,15 +16,42 @@
 #include <iostream>
 #include <fstream>
 #include "/Users/katrinasharonin/Downloads/kerchunkC/code/c++/include/json.hpp"
+#include "config.h"
 
 using namespace std;
 using json = nlohmann::json;
+
+typedef enum {
+    COMPACT_LAYOUT          = 0,
+    CONTIGUOUS_LAYOUT       = 1,
+    CHUNKED_LAYOUT          = 2,
+    UNKNOWN_LAYOUT          = 3
+} layout_t;
+
+/**
+ * @brief split according to character of interest
+ * 
+ * @param s 
+ * @param delimiter 
+ * @return std::vector<std::string> 
+ */
+std::vector<std::string> splitString(const std::string& s, char delimiter) {
+    std::vector<std::string> tokens;
+    std::stringstream ss(s);
+    std::string token;
+    while (std::getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
 
 /**
  * @brief given json path, extract universal metadata valid for all chunks
  * 
  * @param path_to_json 
- * @return std::tuple< std::string, std::string, int, std::string, float, int, std::string, std::string, std::string, int> 
+ * @return std::tuple< std::string, std::string, int, 
+ * std::string, float, int, std::string, std::string, std::string, int> 
  */
 std::tuple< std::string, std::string, int, std::string, float, int, std::string, std::string, std::string, int> readJsonMeta(std::string path_to_json) {
     // extract meta data universal for entire arr
@@ -37,26 +64,70 @@ std::tuple< std::string, std::string, int, std::string, float, int, std::string,
         json jsonData;
         jsonFile >> jsonData;
 
-        // TODO: CONDITION ON VARIABLES AND DATSET PATH
-        // if DATASET PATH not "" {-> set down to reference, then do var search}
-        // if VAR not "" {-> fetch zvar and condition on in, cannot assume "it" is reference start} 
-        
-        // bring from refs down to known group
-        // TODO zgroup addition/recursor
-
-        // access refs 
         if (jsonData.find("refs") != jsonData.end()) {
 
             // .../.ZARRAY 
             auto it = jsonData["refs"].begin();
-            std::advance(it, 2);
-            std::string force_in = jsonData["refs"][it.key()];
-            json zarray = json::parse(force_in);
+            std::string force_in;
+            json zarray;
+
+            // CASE 1: NO DATASET PATH -> VAR MUST MATCH 
+            if (strlen(HARDCODED_DATASET_NAME)==0) {
+                // Allow for a default "" which will take first known, not recommended
+                if (strlen(HARDCODED_VARIABLE)== 0) {
+                    std::cout << "WARNING: no variable provided, assuming first .zarray is variable to use" << std::endl;
+                    // skip zgroup + zattrs
+                    std::advance(it, 2);
+                    force_in = jsonData["refs"][it.key()];
+                    zarray = json::parse(force_in);
+                } 
+                else {
+                    // search for var and apply
+                    std::string zarr_search_term = std::string(HARDCODED_VARIABLE) + "/.zarray"; 
+                    try {
+                        force_in = jsonData["refs"][zarr_search_term];
+                        zarray = json::parse(force_in);
+                    }
+                    catch (const std::exception& e) {
+                        std::cout << "Exception caught in json_parse, var search failed with exception: " << e.what() << std::endl;
+                        std::exit(EXIT_FAILURE);
+                        // throw std::runtime_error("");
+                    }
+
+                }
+            }
+
+            // CASE 2: ITERATE DOWN DATA SET PATH -> THEN ADD VAR FOR ZARR SEARCH
+            else {
+                if (std::string(HARDCODED_DATASET_NAME).front() != '/' || std::string(HARDCODED_DATASET_NAME).back() != '/' ) {
+                    throw std::runtime_error("HARDCODED_VARIABLE must be formatted with front and back slashes e.g.  /quality_assessment/gt2l/");
+                }
+
+                std::string hardcoded_data_path = std::string(HARDCODED_DATASET_NAME);
+                hardcoded_data_path.pop_back();
+                hardcoded_data_path.erase(hardcoded_data_path.begin());
+                std::vector<std::string> terms = splitString(hardcoded_data_path, '/');
+
+                // std::string key = "";
+                jsonData = jsonData["refs"];
+
+                std::string key = std::accumulate(std::next(terms.begin()), terms.end(),
+                                      terms.front(),
+                                      [](const std::string &a, const std::string &b) {
+                                          return a + "/" + b;
+                                      });
+
+                // now apply final zarr fetch on the location
+                force_in = jsonData[key + "/" + HARDCODED_VARIABLE + "/.zarray"];
+                zarray = json::parse(force_in);
+
+            }
+
+            assert(zarray["zarr_format"] == 2);
 
             // chunks
             const nlohmann::json& chunks_arr = zarray["chunks"];
             std::string chunks = chunks_arr.dump();
-
             // dtype
             std::string dtype = zarray["dtype"];
             // fill val
@@ -75,19 +146,17 @@ std::tuple< std::string, std::string, int, std::string, float, int, std::string,
             std::string compressor_id;
             int level;
             double add_offset = 0;
-            double scale_factor = 0;
+            double scale_factor = 1;
 
             // TODO test new extraction vars -> expect non 0 for GOES17
             if (zarray.contains("add_offset")) {
                 add_offset = zarray["add_offset"];
             }
             if (zarray.contains("scale_factor")) {
-                add_offset = zarray["scale_factor"];
+                scale_factor = zarray["scale_factor"];
             }
 
             // filters 
-            // TODO: key issue is filter is changing the keys...
-            // TODO: revist based on Alexey's feedback; placement of zlib here is strange...
             json filters = json::parse((std::string) zarray["filters"][0].dump());
             if (filters.contains("elementsize") && filters.contains("id")) {
                 elementsize = filters["elementsize"];
@@ -98,28 +167,26 @@ std::tuple< std::string, std::string, int, std::string, float, int, std::string,
                 level = filters["level"];
             }
 
-            // TODO: revisit possible null value
-            // TODO: possible exception handling which swaps compressor and filter?
-            // compressor 
+            // for now assume the compressor field cannot be a net null; either in filter or here
             json compressor = json::parse((std::string) zarray["compressor"].dump());
             if (zarray["compressor"].is_null()) {
-                std::string compressor_id = "null";
-                int level = 100000; // NULL;
+                assert(!filters.contains("elementsize") && !filters.contains("id"));   
             }
             else {
                 std::string compressor_id = compressor["id"];
                 int level = compressor["level"];
             }
+            
+            // FOR NOW EXCLUDE ASSUMING NOT IMPORTANT FOR FUNCTIONALITY, TODO REVIST
+
             // .../.ZATTRS
-            std::advance(it, 1);
-            std::string force_inn = jsonData["refs"][it.key()];
-            json zattrs = json::parse(force_inn);
+            // std::advance(it, 1);
+            // std::string force_inn = jsonData["refs"][it.key()];
+            // json zattrs = json::parse(force_inn);
 
-            // _ARRAY_DIMENSIONS - not returned for now
-            const nlohmann::json& arr_dims = zattrs["_ARRAY_DIMENSIONS"];
-            std::string array_dims = arr_dims.dump();
-
-            // other zattrs... exclude for now?
+            // // _ARRAY_DIMENSIONS - not returned for now
+            // const nlohmann::json& arr_dims = zattrs["_ARRAY_DIMENSIONS"];
+            // std::string array_dims = arr_dims.dump();
 
             // TODO: need tor return all new generated vars
             return  std::make_tuple(chunks, 
